9bee130a2d7d9cad3e71929d5d0f3d63d0ee6ab7
joaquincunanan <joaquin.cunanan@gmail.com>
2014-08-20 14:04:07 -0700

    added theft, robbery, drugs, and vehicle theft data. Also added code to render tabbed viewing w/jQuery, but it doesn't quite work.

--- proj3_app/static/index.js
+++ proj3_app/static/index.js
@@ -2,9 +2,21 @@ $( document ).ready(function() {
     console.log("ready!");
     var crime_date=[];
     var assault_count=[];
-    var regression=[];
+    var assault_regression=[];
+    var burglary_count=[];
+    var burglary_regression=[];
+    var theft_count=[];
+    var theft_regression=[];
+    var drugs_count=[];
+    var drugs_regression=[];
+    var robbery_count=[];
+    var robbery_regression=[];
+    var vehicletheft_count=[];
+    var vehicletheft_regression=[];
+
 
     $.when(
+
         $.ajax({
             url: '/crime_date/',
             type: 'GET',
@@ -19,7 +31,7 @@ $( document ).ready(function() {
         }),
 
         $.ajax({
-            url: '/assault_count/',
+            url: '/category_count/ASSAULT',
             type: 'GET',
             dataType: 'json',
             success: function (response) {
@@ -32,21 +44,148 @@ $( document ).ready(function() {
         }),
 
         $.ajax({
-            url: '/cat_regression/'+'ASSAULT',
+            url: '/category_count/'+'ROBBERY',
             type: 'GET',
             dataType: 'json',
             success: function (response) {
-                regression = response;
-                console.log("regression: " + regression);
+                robbery_count = response;
+                console.log('robbery:',robbery_count);
             },
             error: function (response) {
                 console.log(response)
             }
-        })
+        }),
+        $.ajax({
+            url: '/category_count/'+'theft',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                theft_count = response;
+                console.log('theft:',theft_count);
+                console.log('#theft days:',theft_count.length)
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+        $.ajax({
+            url: '/category_count/'+'drugs',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                drugs_count = response;
+                console.log('drugs:',drugs_count);
+                console.log('#drug days:',drugs_count.length)
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+        $.ajax({
+            url: '/category_count/'+'BURGLARY',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                burglary_count = response;
+                console.log('BURGLARY:',burglary_count);
+
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+
+        $.ajax({
+            url: '/category_count/'+'gta',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                vehicletheft_count = response;
+                console.log('vehicle theft:',vehicletheft_count);
+
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+
+        $.ajax({
+            url: '/cat_regression/'+'ASSAULT',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                assault_regression = response;
+                console.log("regression: " + assault_regression);
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+        $.ajax({
+            url: '/cat_regression/' + 'BURGLARY',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                burglary_regression = response;
+                console.log("burglary regression: " + burglary_regression);
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+
+            $.ajax({
+            url: '/cat_regression/'+'theft',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                theft_regression = response;
+                console.log("theft regression: " + theft_regression);
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        }),
+            $.ajax({
+                url: '/cat_regression/'+'drugs',
+                type: 'GET',
+                dataType: 'json',
+                success: function (response) {
+                    drugs_regression = response;
+                    console.log("drugs regression: " + drugs_regression);
+                },
+                error: function (response) {
+                    console.log(response)
+                }
+            }),
+            $.ajax({
+                url: '/cat_regression/'+'ROBBERY',
+                type: 'GET',
+                dataType: 'json',
+                success: function (response) {
+                    robbery_regression = response;
+                    console.log("robbery regression: " + robbery_regression);
+                },
+                error: function (response) {
+                    console.log(response)
+                }
+            }),
+            $.ajax({
+                url: '/cat_regression/'+'gta',
+                type: 'GET',
+                dataType: 'json',
+                success: function (response) {
+                    vehicletheft_regression = response;
+                    console.log("vehicle theft regression: " + vehicletheft_regression);
+                },
+                error: function (response) {
+                    console.log(response)
+                }
+            })
     ).done(
         function() {
 
-              console.log('highchart function ',crime_date, assault_count);
+//              console.log('highchart function ',crime_date, assault_count);
                 $('#container').highcharts({
                     plotOptions:{
                         line: {
@@ -61,7 +200,7 @@ $( document ).ready(function() {
                         x: -20 //center
                     },
                     subtitle: {
-                        text: 'datasf.gov',
+                        text: 'data.sfgov.org',
                         x: -20
                     },
                     xAxis: {
@@ -100,13 +239,318 @@ $( document ).ready(function() {
 
                         },
                         {
-                            name:'regression',
-                            data: regression
+                            name:'assault regression',
+                            data: assault_regression
+                        }
+                    ]
+                })},
+        function() {
+                $('#container2').highcharts({
+                    plotOptions:{
+                        line: {
+                            marker: {
+                                radius: 1
+                            }
+                        }
+
+                    },
+                    title: {
+                        text: 'Burglaries 2014',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'data.sfgov.org',
+                        x: -20
+                    },
+                    xAxis: {
+                        categories: crime_date,
+                        labels:{
+                            step: 20,
+                            rotation: 30,
+//                            maxStaggerLines:10,
+                            tickPixelInterval: 100
+                        }
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: '# burglaries'
+                        },
+                        plotLines: [
+                            {
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }
+                        ]
+                    },
+
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [
+//
+                        {
+                            name:'burglary',
+                            data: burglary_count
+                        },
+                        {
+                            name:'burglary regression',
+                            data: burglary_regression
+                        }
+                    ]
+                });
+            },
+
+        function() {
+                $('#container3').highcharts({
+                    plotOptions:{
+                        line: {
+                            marker: {
+                                radius: 1
+                            }
+                        }
+
+                    },
+                    title: {
+                        text: 'Drugs 2014',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'data.sfgov.org',
+                        x: -20
+                    },
+                    xAxis: {
+                        categories: crime_date,
+                        labels:{
+                            step: 20,
+                            rotation: 30,
+//                            maxStaggerLines:10,
+                            tickPixelInterval: 100
+                        }
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: '# drugs'
+                        },
+                        plotLines: [
+                            {
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }
+                        ]
+                    },
+
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [
+//
+                        {
+                            name:'drugs',
+                            data: drugs_count
+                        },
+                        {
+                            name:'drugs regression',
+                            data: drugs_regression
+                        }
+                    ]
+                });
+            },
+
+
+        function() {
+                    $('#container4').highcharts({
+                        plotOptions:{
+                            line: {
+                                marker: {
+                                    radius: 1
+                                }
+                            }
+
+                        },
+                        title: {
+                            text: 'Robbery 2014',
+                            x: -20 //center
+                        },
+                        subtitle: {
+                            text: 'data.sfgov.org',
+                            x: -20
+                        },
+                        xAxis: {
+                            categories: crime_date,
+                            labels:{
+                                step: 20,
+                                rotation: 30,
+    //                            maxStaggerLines:10,
+                                tickPixelInterval: 100
+                            }
+                        },
+
+                        yAxis: {
+                            title: {
+                                text: '# robbery'
+                            },
+                            plotLines: [
+                                {
+                                    value: 0,
+                                    width: 1,
+                                    color: '#808080'
+                                }
+                            ]
+                        },
+
+                        legend: {
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle',
+                            borderWidth: 0
+                        },
+                        series: [
+                            {
+                                name:'robbery',
+                                data: robbery_count
+                            },
+                            {
+                                name:'robbery regression',
+                                data: robbery_regression
+                            }
+                        ]
+                    });
+                },
+
+        function() {
+                $('#container5').highcharts({
+                    plotOptions:{
+                        line: {
+                            marker: {
+                                radius: 1
+                            }
+                        }
+
+                    },
+                    title: {
+                        text: 'Vehicle Theft 2014',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'data.sfgov.org',
+                        x: -20
+                    },
+                    xAxis: {
+                        categories: crime_date,
+                        labels:{
+                            step: 20,
+                            rotation: 30,
+//                            maxStaggerLines:10,
+                            tickPixelInterval: 100
+                        }
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: '# vehicle theft'
+                        },
+                        plotLines: [
+                            {
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }
+                        ]
+                    },
+
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [
+//
+                        {
+                            name:'vehicle theft',
+                            data: vehicletheft_count
+                        },
+                        {
+                            name:'vehicle theft regression',
+                            data: vehicletheft_regression
+                        }
+                    ]
+                });
+            },
+
+            function() {
+                $('#container6').highcharts({
+                    plotOptions:{
+                        line: {
+                            marker: {
+                                radius: 1
+                            }
+                        }
+
+                    },
+                    title: {
+                        text: 'Theft 2014',
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: 'data.sfgov.org',
+                        x: -20
+                    },
+                    xAxis: {
+                        categories: crime_date,
+                        labels:{
+                            step: 20,
+                            rotation: 30,
+//                            maxStaggerLines:10,
+                            tickPixelInterval: 100
+                        }
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: '# thefts'
+                        },
+                        plotLines: [
+                            {
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }
+                        ]
+                    },
+
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [
+//
+                        {
+                            name:'theft',
+                            data: theft_count
+                        },
+                        {
+                            name:'theft regression',
+                            data: theft_regression
                         }
                     ]
                 });
             }
 
         );
-
+//$(function() {
+//    $( "#tabs" ).tabs();
+//  });
     });
--- proj3_app/views.py
+++ proj3_app/views.py
@@ -28,39 +28,62 @@ def crimeinfo(request):
     print(assault_date,assault_total)
     return HttpResponse(json.dumps(assault_date), content_type='application/json')
 
+
 def test(request):
     return render(request, 'test.html')
+
 
 def test2(request):
     return render(request, 'test2.html')
 
-def test3(request):
 
+def test3(request):
     return render(request, 'test3.html')
+
 
 def crime_date(request):
     assault_date = []
     data = Report.objects.filter(category='ASSAULT').values('date', 'category').annotate(total=Count('incident')).order_by('date','category')
 
     for i in data:
-        # assault_date.append(i['date'].strftime('%m/%d/%Y'))
+
         assault_date.append(i['date'].strftime('%m/%d/%Y'))
     print(assault_date)
     return HttpResponse(json.dumps(assault_date), content_type='application/json')
 
-def assault_count(request):
-    assault=[]
-    data = Report.objects.filter(category='ASSAULT').values('date', 'category').annotate(total=Count('incident')).order_by('date','category')
+
+def category_count(request, cat):
+    cat_array = []
+    if cat == 'theft':
+        # The forward slash requires special management since it causes the URL append to fail.
+        cat = 'LARCENY/THEFT'
+    if cat == 'drugs':
+        # The forward slash requires special management since it causes the URL append to fail.
+        cat = 'DRUG/NARCOTIC'
+    if cat == 'gta':
+        # The space requires special management since it causes the URL append to fail.
+        cat = 'VEHICLE THEFT'
+    data = Report.objects.filter(category=cat).values('date', 'category').annotate(total=Count('incident')).order_by('date','category')
 
     for i in data:
-        assault.append(i['total'])
-    print(assault)
-    return HttpResponse(json.dumps(assault), content_type='application/json')
+        cat_array.append(i['total'])
+    print('category count', cat, cat_array)
+    return HttpResponse(json.dumps(cat_array), content_type='application/json')
 
-def cat_regression(request,cat):
+
+def cat_regression(request, cat):
     from numpy import arange
     from scipy import stats
     import numpy
+    if cat == 'theft':
+        # The forward slash requires special management since it causes the URL append to fail.
+        cat = 'LARCENY/THEFT'
+    if cat == 'drugs':
+        # The forward slash requires special management since it causes the URL append to fail.
+        cat = 'DRUG/NARCOTIC'
+    if cat == 'gta':
+        # The space requires special management since it causes the URL append to fail.
+        cat = 'VEHICLE THEFT'
     data = Report.objects.filter(category=cat).values('date', 'category').annotate(total=Count('incident')).order_by('date','category')
     crime = []
     for i in data:
@@ -72,25 +95,23 @@ def cat_regression(request,cat):
     # a = array([xi,ones(l)])
     slope, intercept, r_value, p_value, std_err = stats.linregress(xi, np_assault)
     print 'r value', r_value
-    print  'p_value', p_value
+    print 'p_value', p_value
     print 'standard deviation', std_err
     print 'slope ',slope
     print 'intercept ',intercept
-    data={}
+    data = {}
     data['slope']=slope
     data['intercept']=intercept
     data['r_value']=r_value
     data['p_value']=p_value
     data['std_err']=std_err
 
-    regression_array=[]
-    # short_array=[]
-    for x in range(0,l):
+    regression_array = []
 
-        y=slope*x+intercept
+    for x in range(0, l):
+        y = slope*x+intercept
         regression_array.append(y)
 
-    # short_array=short_array.append(regression_array[len(regression_array)])
     print regression_array
     return HttpResponse(json.dumps(regression_array), content_type='application/json')
 
