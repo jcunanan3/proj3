8763d152ecfe42184b6b1e376dd7a78483b5d28e
joaquincunanan <joaquin.cunanan@gmail.com>
2014-08-22 10:34:14 -0700

    Removed unneeded crime_report class.

--- proj3_app/models.py
+++ proj3_app/models.py
@@ -14,15 +14,3 @@ class Report(models.Model):
     lat = models.FloatField()
     long = models.FloatField()
 
-class Crime_Report(models.Model):
-    incident = models.CharField(max_length=20)
-    category = models.CharField(max_length=30)
-    description = models.CharField(max_length=50)
-    dayofweek = models.CharField(max_length=12)
-    date = models.DateField()
-    time = models.TimeField()
-    precinct = models.CharField(max_length=20)
-    resolution = models.TextField(max_length=200)
-    location = models.CharField(max_length=40)
-    lat = models.FloatField()
-    long = models.FloatField()
